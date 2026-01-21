644ce0e7da9875c103eb324aee1ad066d34d1474
unknown <setsin2549@gmail.com>
2026-01-21 15:21:41 +0700

    5

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,20 @@
 const comment = "TypeScript is a strongly typed programming language"
 
-function shortenComment(comment: string): string {
-  // Write your code below
+function shortenComment(comment: string):string {
+  if(comment === undefined || comment.trim()==''){
+    return ''
+  }
+  else{
+    for( const args of comment){
+      let result = ''
+      if(args.length>5 && args.length<10){
+        result += args
+        return result
+      }
+      else{ 
+        return result
+      }
+    }
+    return ''
+  }
 }
