8f4d2cdf6aebb399bb4b345b4d8501c9dd59108a
unknown <setsin2549@gmail.com>
2026-01-21 14:36:46 +0700

    2

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,27 @@
 // Write your code below
+const input3 = process.argv[2]
+if(input === undefined || input.trim()==''){
+    process.exit
+}
+else{
+    const number = +input3
+    if(isNaN(number)||number<=0||!Number.isInteger(number)){
+        process.exit
+    }
+    else{
+        for(let i = 1 ; i<=number ; i++){
+            if(i%3==0 && i%5==0){
+                console.log('FizzBuzz')
+            }
+            else if(i%3==0 ){
+                console.log('Fizz')
+            }
+            else if(i%5==0){
+                console.log('Buzz')
+            }
+            else {
+                console.log(i)
+            }
+        }
+    }
+}
--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,24 @@
 // Write your code below
+const input4 = process.argv[2]
+if(input === undefined || input.trim()==''){
+    process.exit
+}
+else{
+    const number = +input4
+    if(isNaN(number)||number<=0||!Number.isInteger(number)){
+        process.exit
+    }
+    else{
+        for(let i = 1 ; i<=number ; i++){
+            let result = ''
+            for(let j = i ; j<=number ; j++){
+                if(j!=i){
+                    result += ''
+                }
+                else {
+                    result += i
+                }
+            }
+        }   
+    }
+}
