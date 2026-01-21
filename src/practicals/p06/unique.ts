47857cf8c8740c5e1ebaa3d99e02e4c4eb920a1c
unknown <setsin2549@gmail.com>
2026-01-21 16:15:28 +0700

    8

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,16 +1,19 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
   const merged = [...arr1 , ...arr2]
-  const x:any[] = merged.map((p)=>{
-    let newarr:any[] = []
-    if(!merged.includes(p)){
-      newarr.splice(Number(p), 0, p);
-    }
-    return newarr
-  })
-  return merged
+  // const x:any[] = merged.map((p)=>{
+  //   let newarr:any[] = []
+  //   if(!merged.includes(p)){
+  //     newarr.splice(Number(p), 0, p);
+  //   }
+  //   return newarr
+  // })
+  // return merged
+  const x:any[] = []
+  let z = merged.map( p => !x.includes(p))
+  
 }
 
-const arr1 = [1, 2, 3, 4];
-const arr2 = [3, 4, 5, 6];
+const arr3 = [1, 2, 3, 4];
+const arr4 = [3, 4, 5, 6];
 
-console.log(getUniqueNumbers(arr1,arr2))
+console.log(getUniqueNumbers(arr3,arr4))
--- src/practicals/p08/albums.ts
+++ src/practicals/p08/albums.ts
@@ -1,4 +1,48 @@
-interface FullAlbum {}
+import axios from 'axios'
+import { title } from 'process'
+
+interface Album {
+    userId:number[],
+    id:number,
+    title:string
+}
+
+interface Photo{
+    albumId:number,
+    id:number,
+    title:string,
+    url:string,
+    thumbnailUrl:string
+
+}
+
+interface FullAlbum {
+    userId:number,
+    id:number,
+    title:string
+    photos:Photo[]
+}
+
+export async function mapPhotoToAlbum(userIds?: number[]): Promise<FullAlbum[]> {
+    try{
+        const PHOTOS_URL = await axios.get<Photo[]>('https://jsonplaceholder.typicode.com/photos');
+        const ALBUMS_URL = await axios.get<Album[]>('https://jsonplaceholder.typicode.com/albums');
+        const photos = PHOTOS_URL.data
+        const albums = ALBUMS_URL.data
+        if(!albums.length||!photos.length) return[]
+        const filteralbum = albums.filter((f) => f.userId == userIds)
+        const mapalbum = filteralbum.map((p) => [{
+        userId : p.userId,
+        id: p.id,
+        title: p.title,
+        photos: albums
+    }])
+    return mapalbum
+    }
+    catch(err){
+        return []
+    }
+}
 
 const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos';
 const ALBUMS_URL = 'https://jsonplaceholder.typicode.com/albums';
