5fb746ebeac0207faa6cdb7bdd2c6e5586fa5052
unknown <setsin2549@gmail.com>
2026-01-21 15:36:54 +0700

    6

--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,5 +1,13 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+  const merged = [...arr1 , ...arr2]
+  merged.map((p)=>{
+    let newarr:any[] = []
+    if(!merged.includes(p)){
+      newarr.splice(Number(p), 0, p);
+    }
+    return newarr
+  })
+  return merged
 }
 
 const arr1 = [1, 2, 3, 4];
